290ee336e07474733c143f216fb25b1a89160946
Enrico <enrizangri@gmail.com>
2023-04-01 11:17:49 +0200

    Intial kernel configuration
    Added some config instructions to cargo
    
    Added some code to _start function that now will print Hello World! in VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,28 @@
 
 use core::panic::PanicInfo;
 
+/// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!"; // Define our static byte string
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+
+    let vga_buffer = 0xb8000 as *mut u8; // We cast the integer to a raw pointer
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            // We include our code inside unsafe block to tell the Rust compiler we are absolutely sure that these operations are valid
+            *vga_buffer.offset(i as isize * 2) = byte; // This is our character
+            *vga_buffer.offset(i as isize *2 + 1) = 0xb; // This is the color byte
+        }
+    }
+
     loop {}
 }
